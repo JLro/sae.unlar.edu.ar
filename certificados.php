d13c5edf0a3d4152fa2181d940ba11dc924321d6
Lucas <kerplunk37@gmail.com>
2024-01-10 13:26:21 -0300

    reorganizando archivos y creación de certificados. (trabajando)

---
+++ certificados.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<!-- meta -->
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="keywords" content="universidad, secretaría, sae, estudiantes, estudiantil, guía, certificados">
+	<meta name="category" content="university, education">
+	<meta name="author" content="Lucas Paz">
+
+	<!-- links -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <!-- Material Style CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@materialstyle/materialstyle@3.1.1/dist/css/materialstyle.min.css" integrity="sha384-TveZ4SBMG9Zwu44Pq5aK2bgL+4CaFRTtx6pSSsxmQKWhIRKoONDSRW+k+NA9A0Gk" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+	<title>Document</title>
+</head>
+<body>
+	
+	<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ complementos/header.php
@@ -0,0 +1,44 @@
+<header class="py-3 mb-4 border-bottom">
+	<div class="container">
+			<div class="d-flex flex-wrap align-items-center justify-content-between">
+				<a href="/" class="d-flex align-items-center mb-2 mb-lg-0 link-body-emphasis text-decoration-none">
+					<img src="img/SAE_HORIZONTAL.png" width="150" alt="">
+				</a>
+				<div class="dropdown text-end">
+					<a href="#" class="d-block link-body-emphasis text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+						<img src="https://github.com/mdo.png" alt="mdo" width="32" height="32" class="rounded-circle">
+					</a>
+					<ul class="dropdown-menu text-small">
+						<li><a class="dropdown-item" href="#">New project...</a></li>
+						<li><a class="dropdown-item" href="#">Settings</a></li>
+						<li><a class="dropdown-item" href="#">Profile</a></li>
+						<li><hr class="dropdown-divider"></li>
+						<li><a class="dropdown-item" href="#">Sign out</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+</header>
+<div class="row">
+	<nav class="navbar navbar-expand-lg bg-body-y">
+		<div class="container">
+			<div class="container-fluid">
+				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarNavDropdown">
+					<ul class="navbar-nav">
+						<?php foreach ($menu as $item => $espacio ) { ?>
+						<li class="nav-item">
+							<a class="nav-link active" aria-current="page" href="<?php echo $espacio ?>"><?php echo $item; ?></a>
+						</li>
+						<?php } ?>
+					</ul>
+				</div>
+			</div>
+		<form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
+			<input type="search" class="form-control" placeholder="Search..." aria-label="Search">
+		</form>
+		</div>
+	</nav>
+</div>
