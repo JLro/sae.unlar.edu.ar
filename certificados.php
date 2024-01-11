5c2a4f9d4a765e1b1755a91669e602f94e7c5cb6
Lucas <kerplunk37@gmail.com>
2024-01-11 00:04:12 -0300

    borrado de archivos innecesarios, modificado archivo de php de certificados, agregadas variables al style.css.

--- certificados.php
+++ certificados.php
@@ -29,18 +29,41 @@
 				<div class="col-lg-8 mb-4 order-0">
 					<!-- inicio primer tarjeta -->
 					<div class="card shadow p-4 mb-3">
+						<h2 class="card-title">Certificado de Alumno Inscripto</h2>
+						<p class="card-text">
+							Este certificado corresponde a los estudiantes que han ingresado a la universidad en el presente año, no adeudando ningún tipo de documentación y generalmente esta disponible a partir del inicio del curso de ingreso, una vez que se les asigna el número de matricula.<br> <br>
+
+							<b>¿Cómo obtengo el certificado?</b> <br>
+
+							Debes descargarlo desde el <a href="https://guarani.unlar.edu.ar/" target="blank">SIU Guaraní</a>, haciendo clic en el menú de “Tramites/solicitud de Certificados” y seleccionando “Certificado de Alumno Regular”.
+							<a id="alumno-regular"></a>
+						</p>
+						<div class="col-md-6 mx-2">
+							<div class="card shadow-none mb-3 tooltips overflow-hidden rounded-top-3 bg-verde">
+								<div class="card-header">
+									<h5 ><i class="fa-solid fa-brain"></i> Recordatorio</h5>
+								</div>
+								<div class="card-body text-light">
+									<p class="card-text">El certificado estará disponible una vez cargada y aprobada la documentación..</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<!-- fin tarjeta -->
+					<!-- inicio segunda tarjeta -->
+					<div class="card shadow p-4 mb-3">
 						<h2 class="card-title">Alumno regular</h2>
 						<p class="card-text">
 							Conforme lo marca el art. 6 del <a href="#">Reglamento de Alumnos</a>, y en referencia al art. 50 de la Ley de Educación Superior 24.521; es Estudiante Regular el alumno que hubiere aprobado por lo menos dos (2) asignaturas durante cada año académico. <br>
 
 							<b>¿Cómo obtengo el certificado?</b> <br>
 
-							Debes descargarlo desde el <a href="#">SIU Guaraní</a>, haciendo clic en el menú de “Tramites/solicitud de Certificados” y seleccionando “Certificado de Alumno Regular”.
+							Debes descargarlo desde el <a href="https://guarani.unlar.edu.ar/" target="blank">SIU Guaraní</a>, haciendo clic en el menú de “Tramites/solicitud de Certificados” y seleccionando “Certificado de Alumno Regular”.
 							<a id="alumno-regular"></a>
 						</p>
 						<div class="col-md-6 mx-2">
 							<div class="card shadow-none bg-transparent mb-3 tooltips overflow-hidden rounded-top-3">
-								<div class="card-header bg-info">
+								<div class="card-header bg-verde">
 									<h5 class="card-title"><i class="fa-solid fa-brain"></i> Recordatorio</h5>
 								</div>
 								<div class="card-body">
@@ -49,8 +72,8 @@
 							</div>
 						</div>
 					</div>
-					<!-- fin primer tarjeta -->
-					<!-- inicio segunda tarjeta -->
+					<!-- fin tarjeta -->
+					<!-- inicio tercer tarjeta -->
 					<div class="card shadow p-4 mb-3">
 						<h2 class="card-title">Examen rendido</h2>
 						<p class="card-text">
@@ -67,10 +90,26 @@
 							</ul>
 						</p>
 					</div>
-					<!-- fin primer tarjeta -->
+					<!-- fin tarjeta -->
+					<!-- inicio cuarta tarjeta -->
+					<div class="card shadow p-4 mb-3">
+						<h2 class="card-title">Libre deuda de la biblioteca</h2>
+						<p class="card-text">
+							<b>¿Cómo obtengo el certificado?</b> <br>
+
+							Debes descargarlo desde el , haciendo clic en el menú de “<u>Tramites/solicitud de Certificados</u>” y seleccionando, “<u>Certificado de Evaluación</u>”.
+							Aquí tendrás dos opciones.
+							<a id="libre-deuda"></a>
+							<ol class="list-group list-group-numbered">
+								<li class="list-group-item">Se requiere el llenado del <a href="https://docs.google.com/forms/d/e/1FAIpQLSdGmLP9xRx26ZjXFqErG6fDFzeZD-_jLHMp8xATWCRn2cZgaw/viewform">siguiente formulario</a>  con el objeto de Solicitar el Certificado de Libre deuda.</li>
+								<li class="list-group-item">La biblioteca constatara que el solicitante no adeude material bibliográfico</li>
+								<li class="list-group-item">Luego se emitirá el Certificado Digital vía mail hacia el correo del solicitante. El mismo será emitido por la Biblioteca Central Ángel María Vargas de la Universidad Nacional de La Rioja.</li>
+							</ol>
+						</p>
+					</div>
 				</div>
 				<div class="col-lg-4 col-md-4 order-1">
-					<div class="card shadow">
+					<div class="card shadow sticky-sm-top">
 							<div class="card-header bg-info">
 								<h5>Contenidos</h5>
 							</div>
@@ -79,6 +118,7 @@
 									<li class="list-group-item"><a href="#alumno-inscripto">Certificado de alumno inscripto</a></li>
 									<li class="list-group-item"><a href="#examen-rendido">Certificado de examen rendido</a></li>
 									<li class="list-group-item"><a href="#alumno-regular">Certificado de alumno regular</a></li>
+									<li class="list-group-item"><a href="#libre-deuda">Certificado de Libre deuda de la Biblioteca</a></li>
 								</ul>
 							</div>	
 						</div>
